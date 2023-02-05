1f2af39fec5ff96412c9415b830107ca6c844da8
DiegoFabbrii <diegofabbri@outlook.com>
2023-02-04 23:15:24 -0300

    feat: add login page

---
+++ src/pages/Login.tsx
@@ -0,0 +1,12 @@
+export function Login() {
+  return (
+    <div>
+      <h1>Fazer login</h1>
+      <form action="">
+        <input type="text" required />
+        <input type="password" required />
+        <input type="submit" value="Entrar" />
+      </form>
+    </div>
+  );
+}
