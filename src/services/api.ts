fded3033b7972e063886a78b9a11f6bc6ffffb8d
DiegoFabbrii <diegofabbri@outlook.com>
2023-03-03 17:22:51 -0300

    refactor: change baseURL in api.ts

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,5 +1,7 @@
 import axios from 'axios';
+import dotenv from 'dotenv';
+dotenv.config();
 
 export const api = axios.create({
-  baseURL: 'http://localhost:3004',
+  baseURL: `${process.env.API_URL}`,
 });
