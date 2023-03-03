f36c20d5b49a5f94a42935e6e7aa51f82430f2b1
DiegoFabbrii <diegofabbri@outlook.com>
2023-03-03 18:34:23 -0300

    fix: fix axios baseUrl

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,5 @@
 import axios from 'axios';
-import dotenv from 'dotenv';
-dotenv.config();
 
 export const api = axios.create({
-  baseURL: `${process.env.API_URL}`,
+  baseURL: import.meta.env.VITE_API_URL,
 });
